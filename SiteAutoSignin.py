d062528e47f79417b78116ed1aca331a30cc2408
lovemaoli <95260952+lovemaoli@users.noreply.github.com>
2022-05-26 13:15:26 +0800

    upd 5.26 finished

--- SiteAutoSignin.py
+++ SiteAutoSignin.py
@@ -423,6 +423,170 @@ except:
     sign.click()
 time.sleep(1)
 print("ios已签到2")
+# huaxiashuyu 3
+wd.get("https://www.huaxiashuyu.com/user-2/")
+try:
+    print("try to close ad for twice click")
+    close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+    close_ad.click()
+    time.sleep(1)
+except:
+    print("there is nothing.")
+time.sleep(3)
+try:
+    exit1 = wd.find_element(By.XPATH,'//*[@id="user-profile"]/div/div[1]/div[2]/ul/li[7]/a')
+    exit1.click()
+    time.sleep(1)
+except:
+    try:
+        wd.refresh()
+        time.sleep(5)
+        print("账号3退出1次")
+        exit1 = wd.find_element(By.XPATH,'//*[@id="user-profile"]/div/div[1]/div[2]/ul/li[8]/a')
+        exit1.click()
+        time.sleep(1)
+    except:
+        try:
+            wd.get("https://www.wanbianios.com/user")
+            time.sleep(5)
+            print("账号3退出2次")
+            exit1 = wd.find_element(By.XPATH,'//*[@id="user-profile"]/div/div[1]/div[2]/ul/li[7]/a')
+            exit1.click()
+            time.sleep(1)
+        except:
+            print("账号3退出3次仍失败")
+print("huaxiashuyu3")
+wd.get('https://www.huaxiashuyu.com/')
+time.sleep(5)
+try:
+    close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+    close_ad.click()
+except:
+    print("关闭失败 尝试第二次")
+    try:
+        wd.refresh()
+        time.sleep(4)
+        close_ad = wd.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/button")
+        close_ad.click()
+    except:
+        try:
+            print("第三次尝试")
+            wd.get('https://www.huaxiashuyu.com/')
+            time.sleep(5)
+            close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+            close_ad.click()
+        except:
+            print("第三次仍失败 放弃关闭弹窗")
+try:
+    login = wd.find_element(By.XPATH,'/html/body/div[1]/header/div/div[4]/div[1]')
+    login.click()
+except:
+    print("huaxia login3出现问题 仍继续运行")
+name = wd.find_element(By.XPATH,'//*[@id="login"]/div/form/div[1]/input')
+name.send_keys(configs["21"])
+pwd = wd.find_element(By.XPATH,'//*[@id="login"]/div/form/div[2]/input')
+pwd.send_keys(configs["22"])
+flogin = wd.find_element(By.XPATH,'//*[@id="login"]/div/form/button')
+flogin.click()
+time.sleep(2)
+
+menu = wd.find_element(By.XPATH,'/html/body/div[1]/header/div/div[4]/div[3]')
+menu.click()
+time.sleep(2)
+try:
+    sign = wd.find_element(By.XPATH,'//*[@id="cao_widget_userinfo-5"]/div/div[1]/div[3]/button/i')
+    sign.click()
+except:
+    print("try to close ad for twice click")
+    close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+    close_ad.click()
+    time.sleep(1)
+    sign = wd.find_element(By.XPATH,'//*[@id="cao_widget_userinfo-5"]/div/div[1]/div[3]/button/i')
+    sign.click()
+time.sleep(1)
+print("ios已签到3")
+# huaxiashuyu 4
+wd.get("https://www.huaxiashuyu.com/user-2/")
+try:
+    print("try to close ad for twice click")
+    close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+    close_ad.click()
+    time.sleep(1)
+except:
+    print("there is nothing.")
+time.sleep(3)
+try:
+    exit1 = wd.find_element(By.XPATH,'//*[@id="user-profile"]/div/div[1]/div[2]/ul/li[7]/a')
+    exit1.click()
+    time.sleep(1)
+except:
+    try:
+        wd.refresh()
+        time.sleep(5)
+        print("账号4退出1次")
+        exit1 = wd.find_element(By.XPATH,'//*[@id="user-profile"]/div/div[1]/div[2]/ul/li[8]/a')
+        exit1.click()
+        time.sleep(1)
+    except:
+        try:
+            wd.get("https://www.wanbianios.com/user")
+            time.sleep(5)
+            print("账号4退出2次")
+            exit1 = wd.find_element(By.XPATH,'//*[@id="user-profile"]/div/div[1]/div[2]/ul/li[7]/a')
+            exit1.click()
+            time.sleep(1)
+        except:
+            print("账号4退出3次仍失败")
+print("huaxiashuyu2")
+wd.get('https://www.huaxiashuyu.com/')
+time.sleep(5)
+try:
+    close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+    close_ad.click()
+except:
+    print("关闭失败 尝试第二次")
+    try:
+        wd.refresh()
+        time.sleep(4)
+        close_ad = wd.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/button")
+        close_ad.click()
+    except:
+        try:
+            print("第三次尝试")
+            wd.get('https://www.huaxiashuyu.com/')
+            time.sleep(5)
+            close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+            close_ad.click()
+        except:
+            print("第三次仍失败 放弃关闭弹窗")
+try:
+    login = wd.find_element(By.XPATH,'/html/body/div[1]/header/div/div[4]/div[1]')
+    login.click()
+except:
+    print("huaxia login2出现问题 仍继续运行")
+name = wd.find_element(By.XPATH,'//*[@id="login"]/div/form/div[1]/input')
+name.send_keys(configs["23"])
+pwd = wd.find_element(By.XPATH,'//*[@id="login"]/div/form/div[2]/input')
+pwd.send_keys(configs["24"])
+flogin = wd.find_element(By.XPATH,'//*[@id="login"]/div/form/button')
+flogin.click()
+time.sleep(2)
+
+menu = wd.find_element(By.XPATH,'/html/body/div[1]/header/div/div[4]/div[3]')
+menu.click()
+time.sleep(2)
+try:
+    sign = wd.find_element(By.XPATH,'//*[@id="cao_widget_userinfo-5"]/div/div[1]/div[3]/button/i')
+    sign.click()
+except:
+    print("try to close ad for twice click")
+    close_ad = wd.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/button")
+    close_ad.click()
+    time.sleep(1)
+    sign = wd.find_element(By.XPATH,'//*[@id="cao_widget_userinfo-5"]/div/div[1]/div[3]/button/i')
+    sign.click()
+time.sleep(1)
+print("ios已签到4")
 # 光板小镇 账号1 试运行
 try:
     wd.get("https://www.clfans.club/")
